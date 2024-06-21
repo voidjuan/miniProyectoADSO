e0947175a4a7decfb005fdec219ddbc0c2dc9771
voidjuan <juan_elopez2@soy.sena.edu.co>
2024-06-21 09:33:30 -0500

    update

--- Menu.dart
+++ Menu.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'dart:io';
 
 void main(List<String> args) {
@@ -8,41 +7,35 @@ void main(List<String> args) {
   String? nombreAlumnos, temasExposicion;
 
   //Entrada de Algoritmo
-  do{
+  do {
     print("Bienvenido a la aplicacion para la gestion de exposiciones");
     print("Elija lo que quiere hacer");
-    print("1. Ingresar alumno(s)");
-    print("2. Ingresar un tema de Exposicion");
+    print("1. Ingresar temas de exposicion)");
+    print("2. Ingresar alumno(s)");
     print("3. Asignacion aleatoria de grupos de Exposicion");
     print("4. Salir de la aplicacion");
     print("***************************");
     opcion = int.parse(stdin.readLineSync()!);
     print("***************************");
-    
 
     //Procesos de algoritmo
     switch (opcion) {
       case 1:
-      print("Ingrese la cantidad de alumnos que desea registrar");
-      cantAlumnos = int.parse(stdin.readLineSync()!);
-      for (var i = 0; i < cantAlumnos; i++) {
-      print("Ingrese el nombre del alumno ${i + 1}");
-      nombreAlumnos = stdin.readLineSync();
-      nombresAlumnos.add(nombreAlumnos); 
-      }
-      print(nombresAlumnos);
-      print("************************");
-      break;
-      case 2:
-      print("Ingrese la cantidad de temas para exponer");
-      cantTemas = int.parse(stdin.readLineSync()!);
-      for (var y = 0; y < cantTemas; y++){
-        print("Ingrese el tema a exponer #${y + 1}");
+        print("Ingrese el nombre del tema que se va a exponer");
         temasExposicion = stdin.readLineSync();
+        print("Ingrese la cantidad de estudiantes asociados al grupo");
+        cantAlumnos = int.parse(stdin.readLineSync()!);
         temasExposiciones.add(temasExposicion);
-      }
-      print(temasExposiciones);
-      print("************************");
+        print(temasExposiciones);
+        print("************************");
+        break;
+      case 2:
+        print("Ingrese el nombre completo del alumno");
+        nombreAlumnos = stdin.readLineSync();
+        nombresAlumnos.add(nombreAlumnos);
+        print("************************");
+        break;
+      case 3:
     }
-  }while (opcion != 4);
+  } while (opcion != 4);
 }
